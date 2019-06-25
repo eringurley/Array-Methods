fe6e8f69ac37fde1883d0f7c2d595b2153705251
Erin Gurley <erinmgurley@gmail.com>
2019-06-25 11:23:17 -0700

    wrote test for findIndex and completed function

--- index.js
+++ index.js
@@ -20,7 +20,18 @@ function filter(array, callback) {
     }
   }
   return filtered;
-
 }
 
-module.exports = { map, filter };
+//findIndex
+function find(array, callback) {
+  for(let i = 0; i < array.length; i ++) {
+    if(callback(array[i])) return i; 
+  }
+  return -1;
+}
+// It returns a number
+// 2. Iterate over array if the item is good, return index
+// 3. Else, continue
+// 4. Return -1 if no matches found
+
+module.exports = { map, filter, find };
--- index.test.js
+++ index.test.js
@@ -1,14 +1,21 @@
-const { map, filter } = require('./index');
+const { map, filter, find } = require('./index');
 
 //describe map
 describe('map array method function test', () => {
   it('returns mapped array awith same length as array', () => {
     const array = [1, 2, 3];
     const squared = map(array, numbers => numbers * numbers);
-    expect(squared).toEqual([1, 4, 9]);
-    //expect(mapped).toEqual(expect.any(Array));
+    expect(squared).toEqual(expect.any(Array));
   });
+
+  it('returns mapped array awith same length as array', () => {
+    const array = [1, 2, 3];
+    const squared = map(array, numbers => numbers * numbers);
+    expect(squared).toEqual([1, 4, 9]);
+  });
+
 });
+
 
 //describe filtered
 describe('filter function', () => {
@@ -33,5 +40,32 @@ it('filters out odd numbers', () => {
   expect(evens).toEqual([2, 4]);
 });
 
+
+//describe findIndex
+describe('find index', () => {
+  it('returns a number', () => {
+    const colors = ['blue', 'red', 'green'];
+    const index = find(colors, color => color == 'red');
+    expect(index).toEqual(expect.any(Number));
+  });
+});
+
+it('returns the index of a matching item', () => {
+  const colors = ['blue', 'red', 'green'];
+  const index = find(colors, color => color == 'red');
+  expect(index).toEqual(1);
+});
+
+it('returns -1 if no match', () => {
+  const colors = ['blue', 'red', 'green'];
+  const index = find(colors, color => color == 'red');
+  expect(index).toEqual(-1);
+});
+
+
+// It returns a number
+// 2. Iterate over array if the item is good, return index
+// 3. Else, continue
+// 4. Return -1 if no matches found
 
 
