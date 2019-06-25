8c271e9c2a4d79a7b8ceb713b0f7ab20e7657be6
Erin Gurley <erinmgurley@gmail.com>
2019-06-25 10:46:47 -0700

    write test for filter function

--- index.test.js
+++ index.test.js
@@ -1,9 +1,37 @@
-const { map } = require('./index');
+const { map, filter } = require('./index');
 
-describe('map array method test', () => {
-  it('map and return new array', () => {
+//describe map
+describe('map array method function test', () => {
+  it('returns mapped array awith same length as array', () => {
     const array = [1, 2, 3];
     const squared = map(array, numbers => numbers * numbers);
     expect(squared).toEqual([1, 4, 9]);
+    //expect(mapped).toEqual(expect.any(Array));
   });
 });
+
+//describe filtered
+describe('filter function', () => {
+  it('returns an array', () => {
+    const numbers = [1, 2, 3];
+    const filtered = filter(
+      numbers,
+      number => number % 2 === 0
+    );
+
+    expect(filtered).toEqual(expect.any(Array));
+  });
+});
+
+it('filters out odd numbers', () => {
+  const numbers = [1, 2, 3, 4];
+  const evens = filter(
+    numbers,
+    number => number % 2 === 0
+  );
+
+  expect(evens).toEqual([2, 4]);
+});
+
+
+
